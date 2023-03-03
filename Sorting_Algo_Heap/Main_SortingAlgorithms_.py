038d16276b051add8d231340ead109d537d4700d
DogukanPolatel <70372233+dogukan1047@users.noreply.github.com>
2023-03-03 18:11:25 +0300

    Heap_Quick_Sort

--- Sorting_Algo_Heap/Main_SortingAlgorithms_.py
+++ Sorting_Algo_Heap/Main_SortingAlgorithms_.py
@@ -56,9 +56,54 @@ class SortingAlgorithms:
         if len(arr) == 1:
             return arr
         midPoint = len(arr) // 2
-        leftpart=arr[:midPoint]
-        rightPart=arr[midPoint:]
-        return self.merge(self.mergeSort(leftpart),self.mergeSort(rightPart))
+        leftpart = arr[:midPoint]
+        rightPart = arr[midPoint:]
+        return self.merge(self.mergeSort(leftpart), self.mergeSort(rightPart))
+
+    def pivot(self, arr, pivotIndex, endIndex):
+        swapIndex = pivotIndex
+        for i in range(pivotIndex + 1, endIndex + 1):
+            if arr[i] < arr[pivotIndex]:
+                swapIndex += 1
+                arr[swapIndex], arr[i] = arr[i], arr[swapIndex]
+        arr[pivotIndex], arr[swapIndex] = arr[swapIndex], arr[pivotIndex]
+        return swapIndex
+
+    def quickSort(self, arr, leftPointer=0, rightPointer=None):
+        if rightPointer == None:
+            rightPointer = len(arr) - 1
+        if leftPointer < rightPointer:
+            pivotIndex = self.pivot(arr, leftPointer, rightPointer)
+            self.quickSort(arr, leftPointer, pivotIndex - 1)
+            self.quickSort(arr, pivotIndex + 1, rightPointer)
+        return arr
+
+    def heapify(self, arr, n, i):
+        largest = i
+        l = 2 * i + 1
+        r = 2 * i + 2
+
+        if l < n and arr[largest] < arr[l]:
+            largest = l
+
+        if r < n and arr[largest] < arr[r]:
+            largest = r
+
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
+            self.heapify(arr, n, largest)
+
+    def heapSort(self, arr):
+        n = len(arr)
+
+        # Build a MAX_HEAP.
+        for i in range(n, -1, -1):
+            self.heapify(arr, n, i)
+
+        # Swap nodes with root to sort
+        for i in range(n - 1, 0, -1):
+            arr[i], arr[0] = arr[0], arr[i]
+            self.heapify(arr, i, 0)
 
 
 sorting = SortingAlgorithms()
