786c16fb6685f8ba829970132aaa2ec9f0303e9d
DogukanPolatel <70372233+dogukan1047@users.noreply.github.com>
2023-03-04 13:03:00 +0300

    Liked_List1.0

--- Linked_List_/Main_Lİnked_List.py
+++ Linked_List_/Main_Lİnked_List.py
@@ -1,22 +1,10 @@
-from Node import Node,DoublyNode
+from Node import Node,DoublyNode,LinkedList
 
-firstNode = Node(10)
-secondNode = Node(15)
-thirdNode = Node(20)
 
-firstNode.nextNode = secondNode
-secondNode.nextNode = thirdNode
-thirdNode.nextNode = None
+linkedList=LinkedList()
+linkedList.InsetAtBeginning(1)
+linkedList.InsetAtBeginning(2)
+linkedList.InsetAtBeginning(3)
+linkedList.InsetAtBeginning(4)
 
-print(firstNode.nextNode.value)  # Second Node değeri
-
-firstNode = DoublyNode(10)
-secondNode = DoublyNode(20)
-thirdNode = DoublyNode(30)
-
-firstNode.nextNode=secondNode
-secondNode.prevNode=firstNode
-secondNode.nextNode=thirdNode
-thirdNode.prevNode=secondNode
-
-print(thirdNode.prevNode.prevNode.value)
+linkedList.printList()
--- Linked_List_/Node.py
+++ Linked_List_/Node.py
@@ -5,7 +5,93 @@ class Node:
 
 
 class DoublyNode():
-    def __init__(self,value):
-        self.value=value
-        self.nextNOde=None
-        self.prevNode=None
+    def __init__(self, value):
+        self.value = value
+        self.nextNOde = None
+        self.prevNode = None
+
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+    def InsetAtBeginning(self, new_value):
+        new_Node = Node(new_value)
+        new_Node.nextNode = self.head
+        self.head = new_Node
+
+    def InsetAfter(self, prev_Node, new_Value):
+
+        if prev_Node is None:
+            print("The given previous node must inLinkedList.")
+            return
+
+        new_node = Node(new_Value)
+        new_node.nextNode = prev_Node.nextNode
+        prev_Node.next = new_node
+
+        # Insert at the end
+
+    def insertAtEnd(self, new_data):
+        new_node = Node(new_data)  # Yeni node olşturduk
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last = self.head
+        while (last.next):
+            last = last.nextNode
+
+        last.nextNode = new_node
+
+        # Deleting a node
+
+    def deleteNode(self, position):
+
+        if self.head is None:
+            return
+
+        temp = self.head
+
+        if position == 0:
+            self.head = temp.nextNode
+            temp = None
+            return
+
+        # Find the key to be deleted
+        for i in range(position - 1):
+            temp = temp.nextNode
+            if temp is None:
+                break
+
+        # If the key is not present
+        if temp is None:
+            return
+
+        if temp.nextNode is None:
+            return
+
+        next = temp.nextNode.nextNode
+
+        temp.nextNode = None
+
+        temp.nextNode = next
+
+    def search(self, key):
+
+        current = self.head
+
+        while current is not None:
+            if current.value == key:
+                return True
+
+            current = current.nextNode
+
+        return False
+
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(str(temp.value) + " ", end="")
+            temp = temp.nextNode
